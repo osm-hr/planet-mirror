da0acd24810c12131d2c602863f71d1bf1b21bc8
hbogner <hbogner@gmail.com>
2020-11-25 16:30:36 +0100

    mkdir xear folder if doesnt exist

--- planet-mirror.sh
+++ planet-mirror.sh
@@ -10,7 +10,7 @@ WEB=/osm/planet-mirror/web
 
 #remove older than 32 days
 find $WEB/pbf -name "planet-*pbf*" -type f -mtime +32 -exec rm -f {} \;
-find $WEB/planet/$YEAR -name "planet-*bz2*" -type f -mtime +32 -exec rm -f {} \;
+find $WEB/planet -name "planet-*bz2*" -type f -mtime +32 -exec rm -f {} \;
 
 #get pbf files
 rsync -lptv planet.openstreetmap.org::planet/pbf/planet-$TODAY*.pbf* $WEB/pbf/
@@ -21,7 +21,11 @@ then
 	rsync -lptv planet.openstreetmap.org::planet/pbf/planet-${LASTWEEK}3*.pbf* $WEB/pbf/
 fi
 
-#get bz2 files
+##get bz2 files
+#if [ ! -d $WEB/planet/$YEAR ]
+#then
+#	mkdir $WEB/planet/$YEAR
+#fi
 #rsync -lptv planet.openstreetmap.org::planet/planet/$YEAR/planet-$TODAY*.bz2* $WEB/planet/$YEAR
 #if [ $TODAY != $LASTWEEK ]
 #then
