5109a331a3b714fe67cb8e2d4bd1b1297474fb82
hbogner <hbogner@gmail.com>
2020-11-25 10:30:31 +0100

    add bz2 downloads

--- planet-mirror.sh
+++ planet-mirror.sh
@@ -3,14 +3,18 @@
 
 TODAY=$(date +"%y%m")
 LASTWEEK=$(date +"%y%m" --date='7 days ago')
-
-echo $TODAY
-echo $LASTWEEK
+YEAR=$(date +"%Y")
+YEARLASTWEEK=$(date +"%Y" --date='7 days ago')
 
 WEB=/osm/planet-mirror/web
 
-rsync -lptv planet.openstreetmap.org::planet/pbf/planet-$TODAY*.pbf* $WEB/pbf
-rsync -lptv planet.openstreetmap.org::planet/pbf/planet-$LASTWEEK*.pbf* $WEB/pbf
+#get pbf files
+rsync -lptv planet.openstreetmap.org::planet/pbf/planet-$TODAY*.pbf* $WEB/pbf/
+rsync -lptv planet.openstreetmap.org::planet/pbf/planet-$LASTWEEK*.pbf* $WEB/pbf/
+
+#get bz2 files
+rsync -lptv planet.openstreetmap.org::planet/planet/$YEAR/planet-$TODAY*.bz2* $WEB/planet/
+#rsync -lptv planet.openstreetmap.org::planet/planet/$YEARLASTWEEK/planet-$LASTWEEK*.bz2* $WEB/planet/
 
 #remove older than 30 days
 find $WEB/planet -name "planet-*pbf*" -type f -mtime +30 -exec rm -f {} \;
