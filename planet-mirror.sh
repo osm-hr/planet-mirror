2e398f2049240763f87994f4aa2b8aa4be02e94c
hbogner <hbogner@gmail.com>
2020-11-25 11:27:46 +0100

    extend delete to 32 days to accomodate 31 day months

--- planet-mirror.sh
+++ planet-mirror.sh
@@ -9,8 +9,8 @@ YEARLASTWEEK=$(date +"%Y" --date='7 days ago')
 WEB=/osm/planet-mirror/web
 
 #remove older than 30 days
-find $WEB/pbf -name "planet-*pbf*" -type f -mtime +30 -exec rm -f {} \;
-#find $WEB/planet -name "planet-*bz2*" -type f -mtime +30 -exec rm -f {} \;
+find $WEB/pbf -name "planet-*pbf*" -type f -mtime +32 -exec rm -f {} \;
+#find $WEB/planet -name "planet-*bz2*" -type f -mtime +32 -exec rm -f {} \;
 
 #get pbf files
 rsync -lptv planet.openstreetmap.org::planet/pbf/planet-$TODAY*.pbf* $WEB/pbf/
