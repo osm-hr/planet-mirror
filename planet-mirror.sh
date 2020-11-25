83dfd431618ac98340f0b732c4a5429e86d1fafc
hbogner <hbogner@gmail.com>
2020-11-25 16:26:26 +0100

    prepare for bz2

--- planet-mirror.sh
+++ planet-mirror.sh
@@ -10,7 +10,7 @@ WEB=/osm/planet-mirror/web
 
 #remove older than 32 days
 find $WEB/pbf -name "planet-*pbf*" -type f -mtime +32 -exec rm -f {} \;
-find $WEB/planet -name "planet-*bz2*" -type f -mtime +32 -exec rm -f {} \;
+find $WEB/planet/$YEAR -name "planet-*bz2*" -type f -mtime +32 -exec rm -f {} \;
 
 #get pbf files
 rsync -lptv planet.openstreetmap.org::planet/pbf/planet-$TODAY*.pbf* $WEB/pbf/
