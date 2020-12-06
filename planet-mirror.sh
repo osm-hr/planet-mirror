85d5bf1f6f3409c1d9fa5d30ae205994f13865b1
Matija Nalis <mnalis-git@voyager.hr>
2020-12-06 03:04:08 +0100

    possibility to get all torrents created, fix some race conditions

--- planet-mirror.sh
+++ planet-mirror.sh
@@ -23,7 +23,8 @@ PREALLOC="none"
 YEAR=$(date +"%Y")
 YEARLASTWEEK=$(date +"%Y" --date='7 days ago')
 WGET_OPT="-q --no-hsts --wait=$WAIT --random-wait"
-ARIA2_OPT="--file-allocation=$PREALLOC --follow-torrent=true --quiet --log=aria2.log --log-level=notice"
+ARIA2_LOG="aria2.$$.log"
+ARIA2_OPT="--file-allocation=$PREALLOC --follow-torrent=true --quiet --log=${ARIA2_LOG} --log-level=notice"
 
 # log text with timestamp, if user wants us to be that $VERBOSE
 logger() {
@@ -55,22 +56,28 @@ fi
 # This is optimized version doing minimal network access
 # if things misbehave after a system crash, you may want to clear $TMP dir
 get_torrent() {
-	SUBDIR=$1
-	HTML=$2
-	MAX=$3
+	PREFIX="$1"
+	EXT="$2"
+	SUB="$3"
+	MAX="$4"
+
+	SUB2="`printf $SUB | tr / _`"
+	HTML="index_${SUB2}.html"
+	SUBDIR="${SUB}/"
 	URL_BASE=https://planet.openstreetmap.org/$SUBDIR
 	DEST_DIR="${WEB}/${SUBDIR}"
+
 	test -d "$DEST_DIR" || mkdir "$DEST_DIR"
 	if [ ! -d "${DEST_DIR}." ]
 	then
-		logger 0 "FATAL ERROR: script error: destination subdirectory $DEST_DIR does not end with /"
+		logger 0 "FATAL ERROR: script error: destination subdirectory $DEST_DIR does not exist"
 		return 33
 	fi
 
-	logger 5 "DEBUG: download/update list of torrents at ${URL_BASE}"
+	logger 5 "DEBUG: download/update list of torrents at ${URL_BASE} to $HTML"
 	wget $WGET_OPT -N --no-if-modified-since --default-page $HTML $URL_BASE
-	NEWEST_TORRENT=$(sed -ne 's/^.*href="\([a-z0-9\.\-]*\.torrent\)".*$/\1/p' $HTML | sort -ru | head -n1)
-	logger 5 "DEBUG: newest torrent determined to be ${NEWEST_TORRENT}"
+	NEWEST_TORRENT=$(sed -ne 's/^.*href="\('${PREFIX}'-[a-z0-9\.\-]*\.torrent\)".*$/\1/p' $HTML | sort -ru | head -n1)
+	logger 5 "DEBUG: newest torrent from $HTML for prefix $PREFIX determined to be ${NEWEST_TORRENT}"
 	NEWEST_FILE=`basename $NEWEST_TORRENT .torrent`
 
 	if [ -z "$NEWEST_TORRENT" ]
@@ -79,45 +86,56 @@ get_torrent() {
 		return 1
 	fi
 
-	if [ -f "${NEWEST_FILE}" ] && fuser -s ${NEWEST_FILE}*
+	if [ -f "${NEWEST_FILE}" -o -f "${NEWEST_FILE}.torrent" -o -f "${NEWEST_FILE}.aria2" ] && fuser -s ${NEWEST_FILE}*
 	then
 		logger 1 "WARNING: another process is using ${NEWEST_FILE}*, skipping download"
 		return 2
 	fi
 
-	if [ -f "$DEST_DIR${NEWEST_FILE}" ]
+	if [ -f "${DEST_DIR}${NEWEST_FILE}" ]
 	then
-		logger 4 "INFO: skipping download of already published $DEST_DIR${NEWEST_FILE}"
+		logger 4 "INFO: skipping download of already published ${DEST_DIR}${NEWEST_FILE}"
 		return 0
 	fi
 
-	# if newest .torrent is not yet downloaded, or if main transfer was interrupted, try downloading (again)
-	if [ ! -f "$NEWEST_TORRENT" -o -f "${NEWEST_FILE}.aria2" ]
-	then
-		# get newest .torrent, and everything contained in it!
-		logger 5 "DEBUG: download/update ${URL_BASE}${NEWEST_TORRENT}"
-		wget $WGET_OPT -N "${URL_BASE}${NEWEST_TORRENT}"
-		logger 5 "DEBUG: aria2 download files from ${URL_BASE}${NEWEST_TORRENT}"
-		aria2c $ARIA2_OPT -x$MAX -s$MAX "${NEWEST_TORRENT}"
+	# get newest .torrent
+	logger 5 "DEBUG: download/update ${URL_BASE}${NEWEST_TORRENT}"
+	wget $WGET_OPT -N "${URL_BASE}${NEWEST_TORRENT}"
 
-		# get newest .md5
-		NEWEST_MD5="${NEWEST_FILE}.md5"
-		logger 5 "DEBUG: download/update MD5 ${URL_BASE}${NEWEST_MD5}"
-		wget $WGET_OPT -N "${URL_BASE}${NEWEST_MD5}"
+	# download FAST everything contained in .torrent
+	logger 5 "DEBUG: aria2 download files from ${URL_BASE}${NEWEST_TORRENT}"
+	aria2c $ARIA2_OPT -x$MAX -s$MAX "${NEWEST_TORRENT}"
 
-		# verify MD5
-		logger 5 "DEBUG: verifying MD5 ${NEWEST_MD5}"
-		md5sum --check --quiet "$NEWEST_MD5" || return 3
-		logger 5 "DEBUG: successful MD5 check of ${NEWEST_MD5}, publishing files"
+	# get newest .md5
+	NEWEST_MD5="${NEWEST_FILE}.md5"
+	logger 5 "DEBUG: download/update MD5 ${URL_BASE}${NEWEST_MD5}"
+	wget $WGET_OPT -N "${URL_BASE}${NEWEST_MD5}"
 
-		mv -f $NEWEST_FILE "$DEST_DIR${NEWEST_FILE}.tmp" && mv -f "$DEST_DIR${NEWEST_FILE}.tmp" "$DEST_DIR${NEWEST_FILE}" && \
-		cp -af $NEWEST_TORRENT $DEST_DIR && \
-		mv -f $NEWEST_MD5  $DEST_DIR && \
-		logger 3 "NOTICE: $NEWEST_FILE downloaded OK."
+	# verify MD5
+	logger 5 "DEBUG: verifying MD5 ${NEWEST_MD5}"
+	md5sum --check --quiet "$NEWEST_MD5" || return 3
+	logger 5 "DEBUG: successful MD5 check of ${NEWEST_MD5}, publishing files"
 
-		mv -f aria2.log aria2.log.old
-	fi
+	mv -f $NEWEST_FILE "$DEST_DIR${NEWEST_FILE}.tmp" && mv -f "$DEST_DIR${NEWEST_FILE}.tmp" "$DEST_DIR${NEWEST_FILE}" && \
+	cp -af $NEWEST_TORRENT $DEST_DIR && \
+	mv -f $NEWEST_MD5  $DEST_DIR && \
+	logger 3 "NOTICE: $NEWEST_FILE downloaded OK."
+
 	return 0
+}
+
+# get yearly version of torrent - just append $YEAR (or $YEARLASTWEEK if newyear) to subdir
+get_torrent_with_year() {
+	Y_PREFIX="$1"
+	Y_EXT="$2"
+	Y_SUB="$3"
+	Y_MAX="$4"
+
+	# get latest torrent in current $YEAR, but if there are none, fall back to $YEARLASTWEEK
+	if ! get_torrent "$Y_PREFIX" "$Y_EXT" "${Y_SUB}/${YEAR}" $Y_MAX
+	then
+		get_torrent "$Y_PREFIX" "$Y_EXT" "${Y_SUB}/${YEARLASTWEEK}" $Y_MAX
+	fi
 }
 
 #
@@ -125,20 +143,25 @@ get_torrent() {
 #
 
 logger 5 "DEBUG: removing files older than $MAXDAYS days"
+find $WEB/pbf -name "history-*.pbf*" -type f -mtime +$MAXDAYS -exec rm -f {} \;
 find $WEB/pbf -name "planet-*.pbf*" -type f -mtime +$MAXDAYS -exec rm -f {} \;
+find $WEB/planet -name "history-*.bz2*" -type f -mtime +$MAXDAYS -exec rm -f {} \;
 find $WEB/planet -name "planet-*.bz2*" -type f -mtime +$MAXDAYS -exec rm -f {} \;
+find $WEB/planet -name "changesets-*.bz2*" -type f -mtime +$MAXDAYS -exec rm -f {} \;
+find $WEB/planet -name "discussions-*.bz2*" -type f -mtime +$MAXDAYS -exec rm -f {} \;
 
 #
 # download files
 #
 
-get_torrent pbf/ index_pbf.html 4
-#get_torrent pbf/full-history/ index_history.html 1
+get_torrent "planet" "pbf" "pbf" 4
+#get_torrent "history" "pbf" "pbf/full-history" 1
 
-# get latest torrent in current $YEAR, but if there are none, fall back to $YEARLASTWEEK
-if ! get_torrent "planet/$YEAR/" "index_planet_bz2_${YEAR}.html" 1
-then
-	get_torrent "planet/$YEARLASTWEEK/" "index_planet_bz2_${YEARLASTWEEK}.html" 1
-fi
+get_torrent_with_year "planet" "bz2" "planet" 1
+#get_torrent_with_year "changesets" "bz2" "planet" 1
+#get_torrent_with_year "discussions" "bz2" "planet" 1
+#get_torrent_with_year "history" "bz2" "planet/full-history" 1
+
+test -f ${ARIA2_LOG} && mv -f ${ARIA2_LOG} aria2.log.old
 
 logger 5 "DEBUG: $0 script finished."
